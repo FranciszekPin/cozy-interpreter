38a71ae983c6f77a518af5db977c38ea0f8d5e93
Franciszek Pindel <franciszek.pin@gmail.com>
2020-12-27 20:53:21 +0100

    Add reading number

--- evaluation.c
+++ evaluation.c
@@ -1,2 +1,30 @@
 #include "evaluation.h"
+#include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
 
+char * readNumber(char *expression, int* number) {
+    int read_number = 0;
+    bool is_negative = false;
+
+    if (*expression == '-') {
+        is_negative = true;
+        expression++;
+    }
+
+    if (!isdigit(*expression)) {
+        return NULL;
+    }
+
+    while (isdigit(*expression)) {
+        read_number = read_number*10 + (*expression - '0');
+        expression++;
+    }
+
+    if (is_negative)
+        read_number *= -1;
+
+    *number = read_number;
+
+    return expression;
+}
--- evaluation.h
+++ evaluation.h
@@ -1,6 +1,8 @@
 #ifndef COZY_INTERPRETER_EVALUATION_H
 #define COZY_INTERPRETER_EVALUATION_H
 
+#include "binary_tree.h"
 
+char * readNumber(char *expression, int* number);
 
 #endif //COZY_INTERPRETER_EVALUATION_H
