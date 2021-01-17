8038992d4e55ccefa220701146fb029d5740e6a9
Franciszek Pindel <franciszek.pin@gmail.com>
2021-01-17 18:32:16 +0100

    Delete CTest testing

--- testing/test_evaluation.h
+++
@@ -1,9 +0,0 @@
-#include <stdbool.h>
-
-#ifndef COZY_INTERPRETER_TEST_EVALUATION_H
-#define COZY_INTERPRETER_TEST_EVALUATION_H
-
-bool test_if_equal_remove_unnecessary_brackets(char *argument, char *expected_value);
-bool test_if_equal_brackets_are_correct(char *argument, bool expected_value);
-
-#endif //COZY_INTERPRETER_TEST_EVALUATION_H
