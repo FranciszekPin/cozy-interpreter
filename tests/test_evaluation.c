7cae1237e246f45377f305f229f09c7dbe69caf9
Franciszek Pindel <franciszek.pin@gmail.com>
2021-01-17 20:36:16 +0100

    Add testing functions

--- tests/test_evaluation.c
+++ tests/test_evaluation.c
@@ -9,6 +9,29 @@ void test_evaluation() {
     test_expression_to_ONP("(((2+(3*5))))", "235*+");
 }
 
+void test_detect_to_which_lexical_unit_character_belongs(char c, lexical_unit_t expected_val) {
+    lexical_unit_t result;
+    result = detect_to_which_lexical_unit_character_belongs(c);
+    run_test("test detecting lexical unit", expected_val == result);
+}
+
+void test_skip_whitespace(char *expression, char *expected_val) {
+    expression = skip_whitespace(expression);
+
+    run_test("test skip_whitespace()", test_if_result_equals_expected(expression, expected_val));
+}
+
+void test_read_lexical_unit(char *expression, char *expected_val) {
+    char word[LINE_LENGTH];
+    read_lexical_unit(expression, word);
+
+    run_test("test read_lexical_unit()", test_if_result_equals_expected(word, expected_val));
+}
+
+void test_end_of_string(char *expression, bool expected_val) {
+    run_test("test end_of_string()", end_of_string(expression) == expected_val);
+}
+
 void test_expression_to_ONP(char *expression, char *expected_val) {
     char ONP[LINE_LENGTH];
     expression_to_ONP(expression, ONP);
