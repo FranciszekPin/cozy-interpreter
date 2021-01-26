f39de40e63079f340baa22a091bef07b64b20676
Franciszek Pindel <franciszek.pin@gmail.com>
2021-01-26 18:50:00 +0100

    Add module for handling source code

--- src/source_code_reader.c
+++ src/source_code_reader.c
@@ -1,9 +1,15 @@
 #include "source_code_reader.h"
+#include "string_operations.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 
-char ** load_source_code(char *file_name, source_code_t* source_code) {
+void create_source_code_reader(source_code_t * source_code) {
+    source_code->act_line_number = 0;
+    source_code->number_of_lines = 0;
+}
+
+void load_source_code(char *file_name, source_code_t* source_code) {
     FILE *file;
 
     file = fopen(file_name, "r");
@@ -13,10 +19,45 @@ char ** load_source_code(char *file_name, source_code_t* source_code) {
     }
 
     int line_number = 0;
-    while (fgets(source_code->code[line_number], 150, file)) {
-        printf("%3d: %s", line_number, source_code->code[line_number]);
+    while (fgets(source_code->code[line_number], LINE_LENGTH, file))
         line_number++;
-    }
+
+    source_code->number_of_lines = line_number;
+
     fclose(file);
+}
+
+char *get_code_line(source_code_t *source_code) {
+    if (source_code->act_line_number >= source_code->number_of_lines) {
+        // TODO: no lines left
+    }
+
+    return source_code->code[source_code->act_line_number];
+}
+
+int get_number_of_lines(source_code_t *sourceCode) {
+    return sourceCode->number_of_lines;
+}
+
+int get_act_line_number(source_code_t *source_code) {
+    return source_code->act_line_number;
+}
+
+void move_to_next_line(source_code_t *source_code) {
+    source_code->act_line_number++;
+}
+
+bool are_lines_to_read(source_code_t *sourceCode) {
+    return get_act_line_number(sourceCode) < get_number_of_lines(sourceCode);
+}
+
+void skip_empty_lines(source_code_t *source_code) {
+    char *act_line = get_code_line(source_code);
+    act_line = skip_whitespace(act_line);
+    while (are_lines_to_read(source_code) && *act_line == '\0') {
+        move_to_next_line(source_code);
+        act_line = get_code_line(source_code);
+        act_line = skip_whitespace(act_line);
+    }
 }
 
--- src/source_code_reader.h
+++ src/source_code_reader.h
@@ -1,13 +1,29 @@
 #ifndef COZY_INTERPRETER_SOURCE_CODE_READER_H
 #define COZY_INTERPRETER_SOURCE_CODE_READER_H
 
+#include <stdbool.h>
 #include "constants.h"
 
 typedef struct {
     char code[MAX_NUMBER_OF_LINES][LINE_LENGTH];
     int number_of_lines;
+    int act_line_number;
 } source_code_t;
 
-char ** load_source_code(char *file_name, source_code_t *source_code);
+void create_source_code_reader(source_code_t * source_code);
+
+char *get_code_line(source_code_t *source_code);
+
+bool are_lines_to_read(source_code_t *sourceCode);
+
+void skip_empty_lines(source_code_t *source_code);
+
+int get_number_of_lines(source_code_t *sourceCode);
+
+int get_act_line_number(source_code_t *source_code);
+
+void move_to_next_line(source_code_t *source_code);
+
+void load_source_code(char *file_name, source_code_t *source_code);
 
 #endif //COZY_INTERPRETER_SOURCE_CODE_READER_H
