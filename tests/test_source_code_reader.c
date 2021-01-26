273c5a9507e27c385ea148fb312e9f946f8f46ef
Franciszek Pindel <franciszek.pin@gmail.com>
2021-01-26 18:51:04 +0100

    Add tests for source code reader

--- tests/test_source_code_reader.c
+++ tests/test_source_code_reader.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include "test_source_code_reader.h"
-#include "../src/source_code_reader.h"
 #include "../src/constants.h"
+#include "tester_kit.h"
+#include "../src/string_operations.h"
 
 void test_source_code_reader() {
     test_read_source_code();
@@ -8,8 +10,33 @@ void test_source_code_reader() {
 
 void test_read_source_code() {
     source_code_t source_code;
-    source_code.number_of_lines = 0;
+    create_source_code_reader(&source_code);
 
+    load_source_code("test.coz", &source_code);
 
-    load_source_code("main.coz", &source_code);
+    run_test("test checking number of lines", get_number_of_lines(&source_code) == 7);
+
+    run_test("test reading from file line 0", equal(get_code_line(&source_code), "some\n"));
+    move_to_next_line(&source_code);
+    run_test("test reading from file line 1", equal(source_code.code[1], "    random\n"));
+    move_to_next_line(&source_code);
+    run_test("test reading from file line 2", equal(source_code.code[2], "    test\n"));
+    move_to_next_line(&source_code);
+    run_test("test reading from file line 3", equal(get_code_line(&source_code), "    here.\n"));
+
+    run_test("test if there are lines to read", are_lines_to_read(&source_code) == true);
+
+    skip_empty_lines(&source_code);
+    run_test("test skipping whitespace", get_act_line_number(&source_code) == 3);
+
+    move_to_next_line(&source_code);
+    skip_empty_lines(&source_code);
+    run_test("test skipping whitespace", get_act_line_number(&source_code) == 6);
+
+    skip_empty_lines(&source_code);
+    run_test("test skipping whitespace", get_act_line_number(&source_code) == 6);
+
+    move_to_next_line(&source_code);
+    skip_empty_lines(&source_code);
+    run_test("test if there are lines to read", are_lines_to_read(&source_code) == false);
 }
--- tests/test_source_code_reader.h
+++ tests/test_source_code_reader.h
@@ -1,8 +1,12 @@
 #ifndef COZY_INTERPRETER_TEST_SOURCE_CODE_READER_H
 #define COZY_INTERPRETER_TEST_SOURCE_CODE_READER_H
 
+
+#include "../src/parser.h"
+
 void test_source_code_reader();
 
 void test_read_source_code();
+
 
 #endif //COZY_INTERPRETER_TEST_SOURCE_CODE_READER_H
