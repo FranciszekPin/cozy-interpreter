5c3fbd75716271a0e21cb9034d29a75dbc59fc02
Franciszek Pindel <franciszek.pin@gmail.com>
2021-02-05 13:55:47 +0100

    Add getting file name from program argument

--- src/main.c
+++ src/main.c
@@ -7,26 +7,32 @@
 #include "program_runner.h"
 #include "../tests/run_tests.h"
 
-int main() {
+int main(int argc, char **argv) {
 
-    run_tests();
+    if (argc == 1) {
+        printf("No file name provided\n");
+    }
+    else if (argc == 2) {
+        source_code_t source_code;
+        create_source_code_reader(&source_code);
+        load_source_code(argv[1], &source_code);
 
-    source_code_t source_code;
-    create_source_code_reader(&source_code);
-    load_source_code("example.coz", &source_code);
-
-    variable_register_t variableRegister = create();
-    instruction_tree_t instruction_tree = NULL;
-
-
-    load_defined_variables(&source_code, variableRegister, instruction_tree);
-    instruction_tree = parse_program(&source_code, instruction_tree, variableRegister);
+        variable_register_t variableRegister = create();
+        instruction_tree_t instruction_tree = NULL;
 
 
-    run_program(instruction_tree, instruction_tree, variableRegister);
+        load_defined_variables(&source_code, variableRegister, instruction_tree);
+        instruction_tree = parse_program(&source_code, instruction_tree, variableRegister);
 
-    remove_variable_register(variableRegister);
-    remove_instruction_tree(instruction_tree);
+
+        run_program(instruction_tree, instruction_tree, variableRegister);
+
+        remove_variable_register(variableRegister);
+        remove_instruction_tree(instruction_tree);
+    }
+    else {
+        printf("Too many arguments\n");
+    }
 
     return 0;
 }
